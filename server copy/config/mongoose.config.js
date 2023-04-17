40e742c2447704c2556c0fbeed0410dba2d438a6
Carlos Raul Benitez Krug <115422555+CarlosRBK@users.noreply.github.com>
2023-04-17 05:36:36 -0400

    Subido por error

--- server copy/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log("Established a connection to the database"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err));
--- server copy/routes/productos.routes.js
+++
@@ -1,9 +0,0 @@
-const { authenticate } = require('../config/jwt.config');
-const ProductoController = require('../controllers/productos.controller');
-
-module.exports = function (app){
-    app.get('/producto', ProductoController.index);
-    app.post('/api/crearProducto', ProductoController.createProducto);
-    app.get('/api/getproductos',authenticate, ProductoController.getproducto);
-    app.get('/api/getallproductos', ProductoController.getallproducto);
-}
